9dac090b0d79d6a3dc11bccf11920ee792b10246
Cosmin Harangus <cosmin@around25.com>
2018-01-21 23:22:45 +0200

    Fixed subscribing to the ticker channel

--- lib/bitfinex_api/websocket.ex
+++ lib/bitfinex_api/websocket.ex
@@ -7,6 +7,11 @@ defmodule BitfinexApi.WebSocket do
     WebSockex.start_link(url, __MODULE__, %{}, name: __MODULE__)
   end
 
+  def handle_connect(_conn, state) do
+    BitfinexApi.WebSocket.Producer.subscribe()
+    {:ok, state}
+  end
+
   def subscribe(client, "ticker") do
     @pairs
     |> Enum.map(&(%{"event" => "subscribe", "channel" => "ticker", "pair" => &1}))
@@ -17,30 +22,24 @@ defmodule BitfinexApi.WebSocket do
     WebSockex.send_frame(client, {:text, Poison.encode!(message)})
   end
 
-  def handle_connect(_conn, state) do
-    {:ok, state}
-  end
-
   # restart on socket connection issues
   def handle_info({:ssl_closed, _}, state) do IO.inspect("SSL Socket closed"); {:stop, :normal, state} end
   def handle_info({:DOWN, _from, :process, _pid, :normal}, state) do IO.inspect("SSL Socket DOWN"); {:stop, :normal, state} end
+  def handle_info({from, msg}, state), do: {:ok, state}
 
   # handle any other valid message from the web socket
   def handle_frame({type, msg}, state) do
-    IO.inspect type
-    IO.inspect msg
     msg = Poison.decode!(msg)
     process_message(msg, state)
   end
+  def handle_frame(msg, state) do IO.inspect(msg); {:ok, state} end
 
   defp process_message(%{"chanId" => chanId, "channel" => "ticker", "event" => "subscribed", "pair" => pair} = msg, state) do
-    IO.inspect msg
     state = Map.merge(state, Map.new([{chanId, pair}]))
     {:ok, state}
   end
   defp process_message([_ | ["hb"]], state), do: {:ok, state}
-  defp process_message([chanId, bid, bid_size, ask, ask_size, daily_change, daily_change_perc, last_price, volume, high, low] = msg, state) do
-    IO.inspect msg
+  defp process_message([chanId, [bid, bid_size, ask, ask_size, daily_change, daily_change_perc, last_price, volume, high, low]], state) do
     ask = ask |> Decimal.new() |> Decimal.to_string(:normal)
     bid = bid |> Decimal.new() |> Decimal.to_string(:normal)
     last_price = last_price |> Decimal.new() |> Decimal.to_string(:normal)
@@ -50,27 +49,27 @@ defmodule BitfinexApi.WebSocket do
     low = low |> Decimal.new() |> Decimal.to_string(:normal)
     high = high |> Decimal.new() |> Decimal.to_string(:normal)
 
-    event = [{Map.get(state, chanId), %{
-              ask: ask,
-              bid: bid,
-              last: last_price,
-              vol_today: %{today: volume, last_24: volume},
-              price: %{today: last_price, last_24: last_price},
-              daily_change: %{amount: daily_change, perc: daily_change_perc},
-              trades: %{today: nil, last_24: nil},
-              low: %{today: low, last_24: low},
-              high: %{today: high, last_24: high},
-              open: nil}
-            }]
-            |> Map.new()
-    IO.inspect event
-#    task = Task.async(BitfinexApi.WebSocket.Producer, :sync_notify, [event])
+    event = %{
+      pair: Map.get(state, chanId),
+      ask: ask,
+      bid: bid,
+      last: last_price,
+      vol_today: %{today: volume, last_24: volume},
+      price: %{today: last_price, last_24: last_price},
+      daily_change: %{amount: daily_change, perc: daily_change_perc},
+      trades: %{today: nil, last_24: nil},
+      low: %{today: low, last_24: low},
+      high: %{today: high, last_24: high},
+      open: nil
+    }
+
+    BitfinexApi.WebSocket.Producer.sync_notify(event)
     {:ok, state}
   end
   defp process_message(%{"event"=> "info"}, state) do {:ok, state} end
   defp process_message(msg, state) do IO.inspect(msg); {:ok, state} end
 
-  def handle_disconnect(%{reason: {:local, reason}}, state), do: {:ok, state}
+  def handle_disconnect(%{reason: {:local, reason}}, state) do IO.inspect(reason); {:ok, state} end
   def handle_disconnect(disconnect_map, state) do
     super(disconnect_map, state)
   end
--- lib/bitfinex_api/websocket/producer.ex
+++ lib/bitfinex_api/websocket/producer.ex
@@ -1,43 +1,45 @@
 defmodule BitfinexApi.WebSocket.Producer do
   use GenStage
 
+  ####################
+  ## PUBLIC METHODS ##
+  ####################
+
   @doc "Starts the broadcaster."
   def start_link() do
     GenStage.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
   def init(:ok) do
-    schedule_work() # Schedule work to be performed on start
     {:producer, {:queue.new, 0}, dispatcher: GenStage.BroadcastDispatcher}
   end
 
-  def handle_info(:work, state) do
-    IO.inspect "=============SUBSCRIBE TO BITFINEX!!!============="
+  def handle_info(:subscribe, state) do
     BitfinexApi.WebSocket.subscribe(BitfinexApi.WebSocket, "ticker")
     {:noreply, [], state}
   end
 
-  defp schedule_work() do
-    Process.send_after(self(), :work, 3 * 1000) # In 10 seconds
+  def subscribe() do
+    Process.send_after(Process.whereis(__MODULE__), :subscribe, 100)
   end
 
   def sync_notify(event, timeout \\ 5000) do
     GenStage.call(__MODULE__, {:notify, event}, timeout)
   end
 
+  ####################
+  ## SERVER METHODS ##
+  ####################
+
   def handle_call({:notify, event}, from, {queue, pending_demand}) do
     queue = :queue.in({from, event}, queue)
     dispatch_events(queue, pending_demand, [])
   end
-
   def handle_demand(incoming_demand, {queue, pending_demand}) do
     dispatch_events(queue, incoming_demand + pending_demand, [])
   end
 
-  defp dispatch_events(queue, 0, events) do
-    {:noreply, Enum.reverse(events), {queue, 0}}
-  end
-
+  defp dispatch_events(queue, 0, events), do: {:noreply, Enum.reverse(events), {queue, 0}}
   defp dispatch_events(queue, demand, events) do
     case :queue.out(queue) do
       {{:value, {from, event}}, queue} ->
